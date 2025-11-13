5db75480f7bda4cbf28728a8703e1e788dd43fb3
rchappali <chappalir@etown.edu>
2025-11-13 09:48:58 -0500

    add us

---
+++ data_src/api/user/addus.php
@@ -0,0 +1,20 @@
+<?php
+
+$rohan = hash("sha256", "3855597");
+echo $rohan;
+
+echo("<br>");
+
+$megan = hash("sha256", "3843408");
+echo $megan;
+
+echo("<br>");
+
+$andrew = hash("sha256", "3847987");
+echo $andrew;
+
+
+
+ 
+
+?>
