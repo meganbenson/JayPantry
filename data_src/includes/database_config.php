6f6a5f499877ff79b6b5676fb78cc29ce379f02a
meganbenson <bensonm@etown.edu>
2025-11-20 08:20:59 -0500

    database connection

--- data_src/includes/database_config.php
+++ data_src/includes/database_config.php
@@ -1,10 +1,18 @@
-<?php
-//connect to the db
+if($_SERVER["HTTP_HOST"]=="127.0.0.1" || $_SERVER["HTTP_HOST"]=="localhost"){
 
-$servername = "127.0.0.1";
-$username = "foodie";
-$password = "yummy";
-$database = "foodpantry";
+   $servername = "127.0.0.1";
+   $username = "root";
+   $password = "";
+   $database = "foodpantry";
+   $GLOBAL_API_KEY = "848429r2g";
+}else{
 
-$GLOBAL_API_KEY = "848429r2g";
+   $servername = "srv557.hstgr.io";
+   $username = "u413142534_bjpantry";
+   $password = "YumF00d!";
+   $database = "u413142534_bjpantrydb";
+
+   $GLOBAL_API_KEY = "848429r2g";
+
+}
 ?>
